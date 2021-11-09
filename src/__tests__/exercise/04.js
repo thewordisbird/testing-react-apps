28a4f93053358c339003e29d5e939719c0644086
Justin Bird <justin@thewordisbird.dev>
2021-11-08 20:24:36 -0800

    form testing complete

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,22 +4,31 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
+// import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+// allows greater extendability. also accepts overrides by default
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
+
+// function buildLoginForm(overrides) {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//     ...overrides,
+//   }
+// }
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm('chucknorris')
+  const {username, password} = buildLoginForm('chucknorris') //If you need a specific override for faker
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
