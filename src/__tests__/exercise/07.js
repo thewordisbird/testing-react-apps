e150f8be3f26064864346718669325ee5f90cff3
Justin Bird <justin@thewordisbird.dev>
2021-11-14 20:48:48 -0800

    ec 3.

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,20 +2,12 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import {render, screen} from '../../test/test-utils'
 import EasyButton from '../../components/easy-button'
-
-function renderWithProviders(ui, {theme = 'light', ...options} = {}) {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  )
-  return render(ui, {wrapper: Wrapper, ...options})
-}
 
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  renderWithProviders(<EasyButton>Easy</EasyButton>)
+  render(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -28,7 +20,7 @@ test('renders with the light styles for the light theme', () => {
 
 test('renders with the dark styles for the dark theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  renderWithProviders(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
